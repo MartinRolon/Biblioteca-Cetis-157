4ad6a54bad686b0cfaf01cfa63f7fcaf4139fcaf
MartinRolon <105063957+MartinRolon@users.noreply.github.com>
2022-11-24 19:08:31 -0600

    Create index.php
    esta es la pagina principal de la biblioteca

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="Biblioteca-Cetis-157">
+    <title>Blioteca Cetis 157</title>
+</head>
+<body>
+   <h1>biblioteca</h1> 
+</body>
+</html>
